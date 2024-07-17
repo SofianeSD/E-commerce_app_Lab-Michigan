d9cc5fb61566e210dd4fa91db0bbd73ad2ef61e5
SofianeSD <sofianesd51@gmail.com>
2024-07-17 16:02:09 -0400

    Implement Observer pattern functionality

--- src/PriceObserver.java
+++ src/PriceObserver.java
@@ -1,6 +1,10 @@
 public class PriceObserver implements OrderObserver {
     @Override
     public void update(Order order) {
-        // Method code
+        if (order.getTotalCost() > 200) {
+            double discountedPrice = order.getTotalCost() - 20;
+            order.setTotalCost(discountedPrice);
+            System.out.println("PriceObserver: Order ID " + order.getId() + " - Applied $20 discount. New total cost: $" + order.getTotalCost());
+        }
     }
 }
